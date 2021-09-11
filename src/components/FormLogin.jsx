e31af13a095807370b72af69615e5369e0ca6178
bomj321 <jmobb612@gmail.com>
2021-09-11 13:33:24 -0400

    first commit

---
+++ src/components/FormLogin.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+const FormLogin = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Correo"
+        name="email"
+        rules={[{ required: true, message: 'Por favor ingresa tu correo' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Por favor ingresa tu contraseña' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Entrar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormLogin;
---
+++ src/containers/Login.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Layout, Row, Col } from 'antd';
+
+import 'antd/dist/antd.css';
+import '../styles/components/Login.css';
+
+import FormLogin from '../components/FormLogin';
+
+const { Header, Footer, Content } = Layout;
+
+const Login = () => {
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content className="content-login">
+        <Row>
+          <Col
+            xs={{ span: 14, offset: 5 }}
+            sm={{ span: 20 }}
+            md={{ span: 8, offset: 7 }}
+            lg={{ span: 8, offset: 7 }}
+          >
+            <FormLogin />
+          </Col>
+        </Row>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Test Vertebra @2021 Created by José ortega
+      </Footer>
+    </Layout>
+  );
+};
+
+export default Login;
